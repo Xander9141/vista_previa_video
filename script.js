060f9a1f40668b04c01a74e145669cd24bf7926f
Xander9141 <ramontorres91.00@gmail.com>
2023-09-22 14:49:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function playVideo() {
+    const video = document.querySelector('video');
+    video.play();
+}
+
+// Función para pausar el video
+function pauseVideo() {
+    const video = document.querySelector('video');
+    video.pause();
+}
